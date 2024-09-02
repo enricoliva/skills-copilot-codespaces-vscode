61904a61c8c6379c750509cef71ca3be97a1d0eb
enricoliva <eoliva@blulink.com>
2024-09-02 08:50:54 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,24 @@
+function skillsMembers() {
+    return [
+        {
+            name: 'John Doe',
+            skills: [
+                'JavaScript'
+            ]
+        },
+        {
+            name: 'Jane Doe',
+            skills: [
+                'JavaScript',
+                'React'
+            ]
+        },
+        {
+            name: 'Jim Doe',
+            skills: [
+                'Node',
+                'MongoDB'
+            ]
+        }
+    ]
+}
